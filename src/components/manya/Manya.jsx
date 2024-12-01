76f807f21f1c96ef6ee41b5c645078fb8d4e2e09
mmehta29 <149706767+mmehta29@users.noreply.github.com>
2024-11-30 18:39:55 -0700

    resume updated

--- src/components/manya/Manya.jsx
+++ src/components/manya/Manya.jsx
@@ -8,7 +8,7 @@ export const Manya = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I am Manya!</h1>
                 <p className={styles.description}>I am a 4th year Computer Science Student at Arizona State University.</p>
-                <a href='https://drive.google.com/file/d/1ft7_zJ3x16rfBAAVkHEDlSROQ-iGaG2H/view?usp=sharing' className={styles.contactBtn}>
+                <a href='https://drive.google.com/file/d/1l28fexFqMzAXPx4or_iEv64QMvOM6W-w/view?usp=sharing' className={styles.contactBtn}>
                     <img src={getImageUrl("paperclip3.png")} alt="paperclip4" className={styles.paperclipImg} />
                     <span className={styles.resumeText}>Resume</span>
 
